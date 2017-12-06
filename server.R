a619aeca13614827ac0fe27be11b03c619a19b22
k-lex <32442780+k-lex@users.noreply.github.com>
2017-12-05 23:42:49 -0800

    Added clarity to first graph and removed commas from second andthird widgets

--- server.R
+++ server.R
@@ -2,7 +2,7 @@ library(shiny)
 library(plotly)
 
 # get all data and take it out of vector form
-soccer.data <- read.csv("~/201-ANM-Final-Project/data/results.csv")
+soccer.data <- read.csv("~/INFO_201/Assignments/201-ANM-Final-Project/data/results.csv")
 soccer.data <- data.frame(lapply(soccer.data, as.character), stringsAsFactors=FALSE)
 soccer.data$home_score <- as.numeric(soccer.data$home_score)
 soccer.data$away_score <- as.numeric(soccer.data$away_score)
@@ -15,30 +15,19 @@ my.server <- function(input, output) {
       # filter data to input specifics
       team.data <- soccer.data %>% filter(home_team == input$teama & away_team == input$teamb)
       f <- list(family = "Courier New, monospace", size = 18, color = "#7f7f7f")
-      x <- list(title = "Date", titlefont = f)
+      x <- list(title = paste0(c(rep("\n&nbsp;", 1), rep("&nbsp;", 1000), "Year", rep("&nbsp;", 1000)), collapse = ""), 
+                titlefont = f)
+      m <- list(l = 50, r = 50, b = 75, t = 50, pad = 4)
       y <- list(title = "Score Differential (+/-)", titlefont = f)
+      t <- list(size = 22, color = 'grey')
       # plot graph
-      pl <- plot_ly(x = team.data$date, y= (team.data$home_score - team.data$away_score),
-        type = "scatter",
-        mode = "markers") %>% layout(title = "Home Team vs Away Team Scores", xaxis = x, yaxis = y)
+      pl <- plot_ly(x = substr(team.data$date, 1, 4), y = (team.data$home_score - team.data$away_score),
+                    type = "scatter",
+                    mode = "markers") %>% layout(title = "Home Team vs Away Team Scores", titlefont = t,
+                                                 xaxis = x, yaxis = y, margin = m)
       print(pl)
     })
-    
-    output$numwins <- renderPlotly({
-      # filter data to input specifics
-      win.data <- soccer.data %>% filter(home_team == input$team_spec)
-      win.data$diff <- win.data$home_score - win.data$away_score
-      # make a new column with just years to group by
-      win.data$year <- format(as.Date(win.data$date, format="%Y-%m-%d"),"%Y")
-      win.data <- win.data %>% filter(diff > 0) %>%
-        filter(date >= input$slide2[1] & date <= input$slide2[2])
-      win.data <- win.data %>% group_by(year) %>% summarise(n = n())
-      # plot graph
-      pw <- plot_ly(win.data, x = ~year, y = ~n, type = 'scatter', 
-                   mode = 'lines')
-      print(pw)
-    })
-    
+
     output$mapmap <- renderPlotly({
       # filter data to input specifics
       us.data <- soccer.data %>% 
@@ -53,9 +42,24 @@ my.server <- function(input, output) {
       } else { # if you do not want it in 3d
           us.data <- us.data %>% group_by(year) %>% summarise(n = n())
           p <- plot_ly(us.data, x = ~year, y = ~n, type = 'scatter', 
-                       mode = 'lines')
+                       mode = 'lines') 
       }
       print(p)
     })
-  
+    
+    
+    output$numwins <- renderPlotly({
+      # filter data to input specifics
+      win.data <- soccer.data %>% filter(home_team == input$team_spec)
+      win.data$diff <- win.data$home_score - win.data$away_score
+      # make a new column with just years to group by
+      win.data$year <- format(as.Date(win.data$date, format="%Y-%m-%d"),"%Y")
+      win.data <- win.data %>% filter(diff > 0) %>%
+        filter(date >= input$slide2[1] & date <= input$slide2[2])
+      win.data <- win.data %>% group_by(year) %>% summarise(n = n())
+      # plot graph
+      pw <- plot_ly(win.data, x = ~year, y = ~n, type = 'scatter', 
+                    mode = 'lines')
+      print(pw)
+    })
 }
